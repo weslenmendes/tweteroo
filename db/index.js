b4e4a66a67081dfb8917816fb4ffeb97ce7cba7d
Weslen Mendes <weslenmends@gmail.com>
2022-04-21 10:25:40 -0300

    feat: implemented a sign-up service

---
+++ db/index.js
@@ -0,0 +1,16 @@
+export const usuarios = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+  },
+];
+
+export const tweets = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    tweet: "eu amo o hub",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 import express from "express";
+import cors from "cors";
+
+import { usuarios, tweets } from "../db/index.js";
 
 const app = express();
 const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/sign-up", (req, res) => {
+  const { username, avatar } = req.body;
+
+  if (!username || !avatar) {
+    return res.status(400).send("Todos os campos são obrigatórios!");
+  }
+
+  const thisUserExists = usuarios.find(
+    (usuario) => usuario.username === username
+  );
+
+  if (!thisUserExists) {
+    usuarios.push({ username, avatar });
+
+    return res.status(201).send("OK");
+  }
+
+  return res
+    .status(422)
+    .send("Esse username já existe, escolha outro, por favor.");
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on: http://localhost:${PORT}/`);
